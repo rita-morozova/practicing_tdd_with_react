b7cecfa935f64b8d380c43438a035cb60b4fcb17
Margarita Morozova <ritamorozova@yahoo.com>
2022-02-15 14:16:44 -0800

    first commit

---
+++ src/components/Gift.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { Button, Form, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+
+class Gift extends Component {
+    constructor() {
+        super();
+
+        this.state = { person: '', present: '' }
+    }
+
+    render() {
+        return (
+          <div className='gift'>
+            <Form>
+              <FormGroup>
+                <FormLabel>Person</FormLabel>
+                <FormControl
+                  className="input-person"
+                  onChange={(event) =>
+                    this.setState({ person: event.target.value })
+                  }
+                ></FormControl>
+              </FormGroup>
+              <FormGroup>
+                <FormLabel>Present</FormLabel>
+                <FormControl
+                  className="input-present"
+                  onChange={(event) =>
+                    this.setState({ present: event.target.value })
+                  }
+                ></FormControl>
+              </FormGroup>
+                </Form>
+                <Button className='btn-remove' onClick={() => this.props.removeGift(this.props.gift.id)}>Remove Gift</Button>
+          </div>
+        );
+    }
+}
+
+export default Gift;
